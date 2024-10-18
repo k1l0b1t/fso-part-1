7dbc500e065c13dbaef9fc43ddab2393a4291b86
Vincent Lammens <vincent@vincentlammens.be>
2024-10-18 16:35:23 +0200

    add ex 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -25,6 +25,8 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive: {(good / (good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
