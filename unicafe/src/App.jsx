4a45b94031527895ee7f48f9ecc7af2bf074d352
Vincent Lammens <vincent@vincentlammens.be>
2024-10-18 16:41:12 +0200

    add ex 1.8, 1.9

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -2,9 +2,30 @@ import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => {
   return (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && bad === 0 && neutral === 0){
+    return (
+      <>
+      <h2> Statistics </h2>
+      No feedback given.
+      </>
+    )
+  }
+  return (
+    <>
+      <h2> Statistics</h2>
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
+      <div>Average: {(good - bad) / (good + neutral + bad)}</div>
+      <div>Positive: {(good / (good + neutral + bad)) * 100} %</div>
+    </>
   )
 }
 
@@ -18,15 +39,10 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good+1)} text='Good'/>
-      <Button handleClick={() => setNeutral(neutral+1)} text='Neutral'/>
-      <Button handleClick={() => setBad(bad+1)} text='Bad'/>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive: {(good / (good + neutral + bad)) * 100} %</p>
+      <Button handleClick={() => setGood(good + 1)} text='Good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
